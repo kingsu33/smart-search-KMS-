67e57e370a4d0741f3d5438f4b6135fe51ef6b66
kingsu33 <2697467@naver.com>
2026-01-21 17:20:28 +0900

    .

--- backend/src/modules/user/users.repository.ts
+++ backend/src/modules/user/users.repository.ts
@@ -2,10 +2,18 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/database/prisma/prisma.service";
 
 @Injectable()
-export class UserRepository{
-    constructor(private readonly prisma: PrismaService) {}
+export class UserRepository {
+    constructor(private readonly prisma: PrismaService) { }
 
     findByEmail(email: string) {
-        
+        return this.prisma.user.findUnique({ where: { email } });
+    }
+
+    findById(id: string) {
+        return this.prisma.user.findId({ where: { id } });
+    }
+
+    createUser(data: { email: string; password: string }) {
+        return this.prisma.user.create({ data });
     }
 }
