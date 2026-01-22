9403f5a3c1a7002aba52f8ca440157a65b6c1414
kingsu33 <2697467@naver.com>
2026-01-22 18:27:07 +0900

    .

---
+++ backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { ConflictException, Injectable, UnauthorizedException } from "@nestjs/common";
+import * as bcrypt from "bcrypt";
+import { UserService } from "../user/users.service";
+
+@Injectable()
+export class AuthService {
+    constructor(private readonly userService: UserService) { }
+
+    async register(email: string, password: string) {
+        const exists = await this.userService.findByEmail(email);
+        if (exists) throw new ConflictException("Email already in use");
+
+        const hash = await bcrypt.hash(password, 12);
+        const user = await this.userService.createUser(email, hash);
+
+        return { id: user.id, email: user.email };
+    }
+
+    async login(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+        if (!user) throw new UnauthorizedException("Invalid Credentials");
+
+        const ok = await bcrypt.compare(password, user.password)
+    }
+}
